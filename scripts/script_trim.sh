34c07ed1a0566f5ebf9c0b1e012a81c1b362a514
Yulong Niu <yulong.niu@hotmail.com>
2019-11-04 17:08:43 +0100

    add trim

---
+++ scripts/script_trim.sh
@@ -0,0 +1,28 @@
+date
+
+RAW_PATH=/netscratch/dep_psl/grp_rgo/yniu/KathrinPersistence/raw_data
+CLEAN_PATH=/netscratch/dep_psl/grp_rgo/yniu/KathrinPersistence/clean_data
+
+FASTP_PATH=/home/yniu/Biotools
+
+CORENUM=16
+
+cd ${RAW_PATH}
+
+## sample names
+fq=($(ls | grep fq.gz))
+fqnames=($(echo "${fq[@]%_*}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+for i in ${fqnames[@]}
+do
+    echo "Trimming ${i}_R1.fq.gz ${i}_R2.fq.gz."
+
+    ${FASTP_PATH}/fastp -w ${CORENUM} \
+                 -z 6 \
+                 -p -c \
+                 -h ${i}.html \
+                 -i ${i}_R1.fq.gz -I ${i}_R2.fq.gz \
+                 -o ${CLEAN_PATH}/${i}_R1.fq.gz -O ${CLEAN_PATH}/${i}_R2.fq.gz
+done
+
+date 
