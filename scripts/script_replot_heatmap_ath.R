7241daae977e1d814d6d8b0ab0a0623f248404a3
Yulong Niu <yulong.niu@hotmail.com>
2019-12-16 11:01:55 +0100

    update color scale

--- scripts/script_replot_heatmap_ath.R
+++ scripts/script_replot_heatmap_ath.R
@@ -41,20 +41,20 @@ scaleC <- rawC %>%
   as_tibble %>%
   bind_cols(rawC %>% select(ID, cl))
 
-cairo_pdf('kmeans_10_ath_heatmap.pdf')
+cairo_pdf('kmeans_10_ath_heatmap2.pdf', height = 10)
 syncom <- HeatmapAnnotation(SynCom = rep(c('AtSC', 'LjSC', 'Mock'), each = 4),
                             col = list(SynCom = c('Mock' = '#1b9e77', 'AtSC' = '#d95f02', 'LjSC' = '#7570b3')),
                             gp = gpar(col = 'black'))
 
 Heatmap(matrix = scaleC %>% select(contains('C_')),
         name = 'Scaled Counts',
-        row_order = order(scaleC$cl) %>% rev,
+        ## row_order = order(scaleC$cl) %>% rev,
         row_split = scaleC$cl,
         row_gap = unit(2, "mm"),
         column_order = 1 : 12,
         column_split = rep(c('AtSC', 'LjSC', 'Mock'), each = 4),
         show_column_names = FALSE,
-        col = colorRampPalette(rev(brewer.pal(n = 10, name = 'Spectral'))[c(-3, -4, -7, -8)])(10),
+        col = colorRampPalette(rev(brewer.pal(n = 7, name = 'RdYlBu')))(100),
         top_annotation = c(syncom))
 dev.off()
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
