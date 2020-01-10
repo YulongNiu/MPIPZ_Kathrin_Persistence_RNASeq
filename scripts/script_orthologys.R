3614e1ca9cd3b629b0f8c4dc6cf477223bf27080
Yulong Niu <yulong.niu@hotmail.com>
2020-01-10 12:56:41 +0100

    add ortho group map

--- scripts/script_orthologys.R
+++ scripts/script_orthologys.R
@@ -2,13 +2,6 @@ library('tidyverse')
 library('limma')
 library('DESeq2')
 
-library('tidyverse')
-library('ComplexHeatmap')
-library('limma')
-library('DESeq2')
-library('RColorBrewer')
-
-
 setwd('/extDisk1/RESEARCH/MPIPZ_Kathrin_Persistence_RNASeq/results_rmfull/')
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~ath DEGs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -77,3 +70,111 @@ heatsigLotus <- (padjsig * fcsig) %>%
 write_csv(heatsigLotus, '../results_orthologs/heatsigLotus.csv')
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+##~~~~~~~~~~~~~~~~~~~compare collaborator best hits~~~~~~~~~~~~~~~~~~~
+library('foreach')
+
+kmeansAth <- read_csv('kmeans_10_ath.csv') %>%
+  select(ID, cl) %>%
+  dplyr::rename(athID = ID, athcl = cl)
+
+kmeansLotus <- read_csv('kmeans_10_lotus_rmfull_rmAtSC.csv')
+
+## extract Best TAIR
+bestTAIR <- kmeansLotus$Best_TAIR %>%
+  strsplit(split = ' ', fixed = TRUE) %>%
+  sapply('[[', 1) %>% {
+    logIdx <- is.na(.)
+    .[logIdx] <- kmeansLotus$ID[logIdx]
+    return(.)
+  }
+
+kmeansLotus <- tibble(lotusID = kmeansLotus$ID,
+                      BestTAIR = bestTAIR,
+                      lotuscl = kmeansLotus$cl)
+
+mergeKmeans <- inner_join(kmeansAth, kmeansLotus, by = c('athID' = 'BestTAIR'))
+
+ath2lotus <- foreach(i = 1:10) %do% {
+  mergeKmeans %>%
+    filter(athcl %in% i) %>%
+    .$lotuscl %>%
+    table %>%
+    sort(decreasing = TRUE)
+}
+
+names(ath2lotus) <- 1:10
+
+lotus2ath <- foreach(i = 1:10) %do% {
+  mergeKmeans %>%
+    filter(lotuscl %in% i) %>%
+    .$athcl %>%
+    table %>%
+    sort(decreasing = TRUE)
+}
+
+names(lotus2ath) <- 1:10
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~orthogroups~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library('magrittr')
+library('stringr')
+library('foreach')
+
+## format orthogroups
+orthoGFile <- '/extDisk1/RESEARCH/MPIPZ_Kathrin_Persistence_RNASeq/orthology_prediction_AtLj/AtLCollaborator/Results_Nov11/Orthogroups.csv'
+
+
+orthoAnno <- readLines(orthoGFile) %>%
+  lapply(function(x) {
+    eachElem <- strsplit(x, split = '\t', fixed = TRUE) %>%
+      unlist %>%
+      .[nchar(.) > 0] ## remove empty
+
+    return(eachElem)
+  }) %>%
+  .[sapply(., length) > 2] %>% ## remove single species
+  sapply(function(x) {
+    eachElm <- strsplit(x, split = ', ', fixed = TRUE) %>%
+      unlist %>%
+      str_trim
+
+    return(eachElm)
+  }) %>%
+  lapply(function(x) {
+    tibble(ID = x[-1],
+           Orthogroup = x[1])
+  }) %>%
+  bind_rows
+
+kmeansAth <- read_csv('kmeans_10_ath.csv') %>%
+  select(ID, cl) %>%
+  dplyr::rename(athID = ID, athcl = cl) %>%
+  inner_join(orthoAnno, by = c('athID' = 'ID'))
+
+kmeansLotus <- read_csv('kmeans_10_lotus_rmfull_rmAtSC.csv') %>%
+  select(ID, cl) %>%
+  dplyr::rename(lotusID = ID, lotuscl = cl) %>%
+  inner_join(orthoAnno, by = c('lotusID' = 'ID'))
+
+mergeKmeans <- inner_join(kmeansAth, kmeansLotus)
+
+ath2lotus <- foreach(i = 1:10) %do% {
+  mergeKmeans %>%
+    filter(athcl %in% i) %>%
+    .$lotuscl %>%
+    table %>%
+    sort(decreasing = TRUE)
+}
+
+names(ath2lotus) <- 1:10
+
+lotus2ath <- foreach(i = 1:10) %do% {
+  mergeKmeans %>%
+    filter(lotuscl %in% i) %>%
+    .$athcl %>%
+    table %>%
+    sort(decreasing = TRUE)
+}
+
+names(lotus2ath) <- 1:10
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
